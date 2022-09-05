5a73605365eaf19a04e57023d5a1e45c88762695
PurplProto <pairedprototype@hotmail.com>
2022-09-05 14:32:26 +0100

    Build markdown table and print it

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { markdownTable } from 'markdown-table';
+import type { Param } from './models';
 import { ParamsParser } from './params-parser';
 import { StringTableParser } from './string-table-parser';
 
@@ -7,14 +9,37 @@ class Main {
     private paramsParser: ParamsParser,
   ) { }
 
-  public buildTable() {
-    this.extractParams();
+  public printTable() {
+    const params = this.extractParams();
+
+    const headers = [
+      'Param',
+      'Title',
+      'Default',
+      'Description: Value'
+    ];
+    const rows = params.map(({ id, title, defaultValue, values }) => [
+      id,
+      title,
+      `\`${defaultValue}\``,
+      values.map(({ name, value }) => `${name}: \`${value}\``).join('\n'),
+    ]);
+
+    const mdTable = markdownTable(
+      [headers, ...rows],
+      {
+        alignDelimiters: true,
+        delimiterEnd: true,
+        delimiterStart: true,
+        padding: true
+      },
+    );
+    console.log(mdTable);
   }
 
-  private extractParams() {
+  private extractParams(): Param[] {
     this.paramsParser.stringTable = this.stringTableParser.parse();
-    const params = this.paramsParser.parse();
-    console.log(params);
+    return this.paramsParser.parse();
   }
 }
 
@@ -22,4 +47,4 @@ const main = new Main(
   new StringTableParser('uncompiled/co40_domination_4_64_blufor.altis/stringtable.xml'),
   new ParamsParser('uncompiled/co40_domination_4_64_blufor.altis/description.ext'),
 );
-main.buildTable();
+main.printTable();
